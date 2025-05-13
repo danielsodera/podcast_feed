90f04ba290280432811d783f7ec62fd7d65265d2
Danielsodera <daniel_sodera@me.com>
2025-05-12 20:45:19 -0400

    Title, publish Data and link added

--- feed.rb
+++ feed.rb
@@ -5,11 +5,14 @@ url = 'https://anchor.fm/s/bb989624/podcast/rss'
 
 URI.open(url) do |rss|
   feed = RSS::Parser.parse(rss)
-  puts "Title: #{feed.channel}"
+ # puts "Title: #{feed.channel.title}"
 
-  feed.items.each do |item|
-    puts "Item: #{item.title}"
-  end
-
+ # puts "Episode: #{feed.items[0].title}"
+  puts feed.channel.items[0].title 
+  puts feed.channel.items[0].pubDate 
+  puts feed.channel.items[0].link 
+  
 end
+
+
 
